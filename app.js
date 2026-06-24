f1bfbcb388fceb7a9c6f7b7f6b28446ff9de0f8e
Kauê Marques <kaue3636q@gmail.com>
2026-06-24 00:14:34 -0300

    Versão Inicial

---
+++ app.js
@@ -0,0 +1,81 @@
+const r11 = document.getElementById("result11");
+const r12 = document.getElementById("result12");
+const r21 = document.getElementById("result21");
+const r22 = document.getElementById("result22");
+
+const grafico1 = document.getElementById("grafico1").getContext('2d');
+const grafico2 = document.getElementById("grafico2").getContext('2d');
+
+let g1 = null, g2 = null;
+
+let dados = [];
+carregarDados().then(json => dados = json);
+
+async function carregarDados() {
+    const response = await fetch('data/dados.json');
+    return await response.json();
+}
+
+document.getElementById('comparar').addEventListener('click', async () => {
+
+
+    const idOpcao1 = document.getElementById('area').value;
+    const idOpcao2 = document.getElementById('area2').value;
+
+    const curso1 = dados.find(item => item.codigo === idOpcao1);
+    const curso2 = dados.find(item => item.codigo === idOpcao2);
+
+    if (curso1){
+        r11.textContent =  `HOMENS: ${curso1.qtdM}`
+        r12.textContent =  `MULHERES: ${curso1.qtdF}`
+
+
+        if (g1) g1.destroy();
+
+        g1 = new Chart(grafico1, { 
+            type: 'pie',
+            data: {
+                labels: ['Homens', 'Mulheres'],
+                datasets: [{
+                    label: 'Distribuição de gênero',
+                    data: [curso1.qtdM, curso1.qtdF],
+                    backgroundColor: [
+                        '#36A2EB',
+                        '#FF6384'
+                    ],
+                }]
+            }
+        });
+    }
+    else {
+        r11.textContent =  `HOMENS: ----`
+        r12.textContent =  `MULHERES: ----`
+    }
+
+    if (curso2){
+        r21.textContent =  `HOMENS: ${curso2.qtdM}`
+        r22.textContent =  `MULHERES: ${curso2.qtdF}`
+
+        if (g2) g2.destroy();
+
+        g2 = new Chart(grafico2, { 
+            type: 'pie',
+            data: {
+                labels: ['Homens', 'Mulheres'],
+                datasets: [{
+                    label: 'Distribuição de gênero',
+                    data: [curso2.qtdM, curso2.qtdF],
+                    backgroundColor: [
+                        '#36A2EB',
+                        '#FF6384'
+                    ],
+                }]
+            }
+        });
+    }
+    else {
+        r21.textContent =  `HOMENS: ----`
+        r22.textContent =  `MULHERES: ----`
+    }
+
+});
